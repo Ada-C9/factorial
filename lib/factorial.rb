702e0eaca4a19c02989e771ad2e41f9d2bdeedec
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-23 20:04:04 -0700

    Written, with minimal space complexity, and passing all tests.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,13 +1,20 @@
 
 require 'pry'
+
+
 # Computes factorial of the input number and returns it
 def factorial(number)
+  accumulator = 1
   if number.nil?
     raise ArgumentError.new("You can't factorialize 'nil.' ")
   elsif
     number == 0 || number == 1
     return 1
   else
-    #CORE OF THE METHOD GOES HERE
+    until number == 0 do
+      accumulator = number * accumulator
+      number -= 1
+    end
   end
+  return accumulator
 end
