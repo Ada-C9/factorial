effd3643265da5351610e46fca6abb8a7aaaf559
Dikla Aharoni Rikovitch <dikla.aharoni@gmail.com>
2018-03-24 13:27:00 -0700

    finished factorial method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,16 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  return 1 if number == 0 || number == 1
+  if number == nil
+    raise ArgumentError.new("nil")
+  end
+
+  factoricl_num = 1
+  counter = 0
+
+  while number != counter
+    factoricl_num *= number - counter
+    counter += 1
+  end
+    return factoricl_num
 end
