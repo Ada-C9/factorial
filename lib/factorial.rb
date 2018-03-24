732ab470d205d0f98341797ea0fc9ab535706ada
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-23 19:31:46 -0700

    Added pry, plus desired minitest features and gems, to the spec file.  Added a pry requirement and the basic edgecase compliance conditionals to the production code. Nothing of substance has been worked out yet, however.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,13 @@
+
+require 'pry'
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number.nil?
+    raise ArgumentError.new("You can't factorialize 'nil.' ")
+  elsif
+    number == 0 || number == 1
+    return 1
+  else
+    #CORE OF THE METHOD GOES HERE
+  end
 end
