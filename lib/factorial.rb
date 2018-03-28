68b7ae0e97fef8ce7d76869e8d0d12e99f55a155
Jackie <jonofre16@hotmail.com>
2018-03-28 16:33:23 -0700

    Jackie - factorial - Ampers

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,22 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number.class != Integer
+    raise ArgumentError.new("Argument must be Integer")
+  elsif number == 0
+    return 1
+  elsif number == 1
+    return 1
+  end
+
+  total = 1
+  n = number
+  y = 0
+
+  number.times do
+    total *= n - y
+    y += 1
+  end
+
+  return total
+
 end
