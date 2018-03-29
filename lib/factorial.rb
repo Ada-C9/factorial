0cd674e4815e1ecbf7bf825b6273319d8d76a769
Hannah Cameron <hannahiscoding@gmail.com>
2018-03-28 18:44:30 -0700

    passing all nominal and edge tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,15 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+
+  raise ArgumentError.new('Nil is not an integer') if number == nil
+
+  return 1 if number == 0
+  return 1 if number == 1
+
+  n = number
+  until n == 1
+    number = number * (n - 1)
+    n -= 1
+  end
+  return number
 end
