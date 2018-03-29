c1bcd084b62acbfeb2f4f09a0de5581b74f97113
Anne <annehwatson@gmail.com>
2018-03-29 14:39:09 -0700

    implemented factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,17 @@
 # Computes factorial of the input number and returns it
+
 def factorial(number)
-  raise NotImplementedError
+  return 1 if number == 0 || number == 1
+
+  if number.class != Integer
+    raise ArgumentError.new("Must be an integer")
+  end
+
+  result = 1
+
+  while number > 0
+    result *= number
+    number -= 1
+  end
+  return result
 end
