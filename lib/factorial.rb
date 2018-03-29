ea8c40a2b8ca032d39f3977be518782a155c2b0b
Kat <kat.eastman@gmail.com>
2018-03-29 14:55:34 -0500

    not quite there.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,32 @@
+require 'pry'
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number == nil
+    raise ArgumentError.new("Nil is not an integer")
+  end
+
+  if number <= 1
+    return number
+  end
+
+
+  result = 0
+  step = number - 1
+
+  iteration = number % 10000
+
+
+  iteration.times do
+    if result == 0
+      result += number * step
+    end
+
+    result = result * step
+
+    step -= 1
+
+    binding.pry
+  end
+
+  return result
 end
