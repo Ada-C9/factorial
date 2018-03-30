f141a71f5401df6700b8b5828b6bf6502d2611e8
Kat <kat.eastman@gmail.com>
2018-03-29 23:51:39 -0500

    playing code golf.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -5,17 +5,11 @@ def factorial(number)
     raise ArgumentError.new("ArgumentError: #{number} is not an integer")
   end
 
-  if number == 1 || number == 0
-    return number ** 0
-  end
-
-  result = number
-  step = number - 1
-
-  until step == 0
-      result *= step
-
-      step -= 1
+  result = number ** 0
+  
+  until number == 0
+    result *= number
+    number -= 1
   end
 
   return result
