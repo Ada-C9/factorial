9b054bce18c4cc2d4c7e1918d382cae183d2a37c
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-29 18:42:51 -0700

    refactored method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -2,8 +2,10 @@
 def factorial(number)
   raise ArgumentError.new("not a number") if number == nil
   evaluated_factorial = 1
-  number.times do |x|
-    evaluated_factorial *= number - x
+  i = 0
+  while i < number
+    evaluated_factorial *= number - i
+    i += 1
   end
   evaluated_factorial
 end
