abcb12271a5b20a7110f362784ad03bd16ab975e
Tor Shimizu <torshimizu@gmail.com>
2018-03-29 17:36:24 -0700

    passing tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,13 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil || number < 0
+
+  return 1 if number < 2
+
+  final = number
+  (number - 1).times do
+    final = final * (number - 1)
+    number -= 1
+  end
+  return final
 end
