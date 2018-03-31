7ee1b7942ffcea57d5ea025935ad2c6b95a38a6b
AngelaPoland <angelalpoland@gmail.com>
2018-03-30 17:07:56 -0700

    completed and passed tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,18 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+
+  if number.class != Integer || number < 0 || number == nil
+    raise ArgumentError.new("#{number} is either not a positive number")
+  end
+
+  if number == 0
+    return 1
+  else
+    i = number - 1
+    while i > 0
+      number *= i
+      i -= 1
+    end
+    return number
+  end
 end
