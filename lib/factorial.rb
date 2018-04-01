f6ce8c8916669d6e39fcfd017f021b559aba6be4
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-01 16:13:42 -0700

    pass tests

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,6 @@
 # Computes factorial of the input number and returns it
+
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError.new("invalid number") if number.nil?
+  (1..number).inject(1, :*)
 end
