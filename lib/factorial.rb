4ae38be4311b7e85dc35b9106adbe542cada7f59
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-04-01 15:03:28 -0700

    Passing all tests, did not include a portion for negative numbers as there was no indicated test for it

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,13 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number == nil
+    raise ArgumentError.new('You must choose a non-negative number')
+  end
+  sum = 1
+  if number > 1
+    (2..number).each do |n|
+      sum *= n
+    end
+  end
+  return sum
 end
