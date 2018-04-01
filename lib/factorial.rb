51e28ddba75ef3a313caa257d7d12a930133cc12
Lily Sky <lilyxsky@gmail.com>
2018-04-01 13:15:31 -0700

    tests pass

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,15 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError.new if number.nil? || number < 0
+  return 1 if number == 0
+
+  answer = 1
+  n = number
+
+  until n < 2
+  answer = answer * (n)
+  n -= 1
+  end
+
+  return answer
 end
