fae5ef51969a6a17a752485844170c343e07e168
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-04-01 13:41:32 -0700

    Updated code and test as well. Passed all tests.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,16 @@
+# 4/01/18
+
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError.new("Invalid number") if number == nil
+  return 1 if number == 0
+
+  # binding.pry
+  final_number = number
+
+  (number - 1).times do
+    number -= 1
+    final_number *= number
+  end
+  return final_number
 end
