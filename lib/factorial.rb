7c18ecd68d741fa0356132afdd6c0c0c97890588
Nora Peters <norapeters5@gmail.com>
2018-04-01 20:12:28 -0700

    refactors line 7. all tests still passing

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -4,7 +4,7 @@ def factorial(number)
   return 1 if number == 0 || number == 1
   result = number
   while number > 2
-    result = result * (number - 1)
+    result *= (number - 1)
     number -= 1
   end
   result
