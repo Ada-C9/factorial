ed63a372272cd8cc611910a6da499fb5a3aefc75
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-02 00:00:14 -0700

    Write functionality for factorial method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,18 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number.nil?
+    raise ArgumentError.new('Cannot compute factorial for nil.')
+  end 
+
+  total = 1
+
+  while number > 0
+    total *= number
+    number -= 1
+  end
+
+  return total
+
 end
+
+puts factorial(0)
