46a3d5f64b3ee4c435f4859264103974ccda1bfb
Abiaina <abinnet.a@gmail.com>
2018-04-02 23:06:12 -0700

    computes factorial for integers > 0, raises argument error for nik

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,16 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number == 0 || number == 1
+    return 1
+  elsif number == nil
+    raise ArgumentError.new
+  end
+
+  factorial = number
+  until number == 1
+    number -= 1
+    factorial = factorial * number
+  end
+
+  return factorial
 end
