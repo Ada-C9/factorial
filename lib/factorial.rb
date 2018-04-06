5d7e602988e9a6c60bb2bb8879c3711108d61521
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-06 07:55:39 -0700

    Employed two approaches

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -3,15 +3,20 @@ def factorial(number)
   #   0! = 1
   # n! = n * (n-1)! if n > 0
   #  = *n * (n-1) * (n-2) * ... * 1*
-  return 1 if number == 0
-
-  total = 0
-  n = number
-
-  while n > 0
-     n * (n-1)
-     n -= 1
+  if number.nil?
+    raise ArgumentError
   end
 
-  return
+  return 1 if number == 0
+
+  total = 1
+  n = number
+
+  while n > 1
+    total *= n
+
+    n -= 1
+  end
+
+  return total
 end
