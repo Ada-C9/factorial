e121ba7cd488e7b58b9efc6073015159231e6537
Cara <cararbrennan@gmail.com>
2018-04-07 14:43:19 -0700

    final version all tests passing

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,14 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  return 1 if number  == 1
+  return 1 if number == 0
+  raise ArgumentError if number == nil
+
+quotient = 1
+i = number
+  while i > 0
+    quotient *= i
+    i -=1
+  end
+  return quotient
 end
