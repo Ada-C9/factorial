cac98cb7d81f2f21ad1fcb4ccd988781a4ef8906
Samantha Berk <sberk10@gmail.com>
2018-04-09 20:10:02 -0700

    All tests passing

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,19 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  if number == nil || number < 0
+    raise ArgumentError.new("Must be a valid number greater than or equal to 0")
+  end
+
+  if number == 0
+    return 1
+  end
+
+  result = number
+  until number == 1
+    number -= 1
+    result = result * (number)
+  end
+  return result
 end
+
+# p factorial(9)
