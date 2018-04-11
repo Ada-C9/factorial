9d53cf93fdb5f666a9e195f4affcf717a2176b0d
Mariko <marikoanderson@gmail.com>
2018-04-10 22:42:59 -0700

    all tests passing

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,4 +1,15 @@
 # Computes factorial of the input number and returns it
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError.new("Invalid input") if number == nil
+
+  return 1 if number == 0
+
+  factorial_answer = number
+
+  (number - 1).times do |num|
+    number -= 1
+    factorial_answer *= number
+
+  end
+  return factorial_answer
 end
